5576ac0c72508ea2962a1f0e430c160efdaaf537
AHMED NABIB <nabibpallab@gmail.com>
2022-10-28 23:08:46 +0600

    refactor

--- header.php
+++ header.php
@@ -7,8 +7,7 @@
     <!-- The above 3 meta tags *must* come first in the head; any other head content must come *after* these tags -->
     <title>News Site</title>
     <!-- Bootstrap -->
-    <!-- <link rel="stylesheet" href="css/bootstrap.min.css" /> -->
-    <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.2.0/dist/js/bootstrap.bundle.min.js" integrity="sha384-A3rJD856KowSb7dwlZdYEkO39Gagi7vIsF0jrRAoQmDKKtQBHUuLZ9AsSv4jD4Xa" crossorigin="anonymous"></script>
+    <link rel="stylesheet" href="css/bootstrap.min.css" />
     <!-- Font Awesome Icon -->
     <link rel="stylesheet" href="css/font-awesome.css">
     <!-- Custom stlylesheet -->
